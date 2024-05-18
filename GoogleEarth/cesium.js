41ee86dcd5097f3ad8773437ec49455f866040e9
QY <marvinyao@icloud.com>
2024-05-18 19:08:53 +0200

    Added Photos

---
+++ GoogleEarth/cesium.js
@@ -0,0 +1,197 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODE1NTVjOS02MzI1LTQ4NTUtYmJmNS1kODAxNzRmODY2MjkiLCJpZCI6MjEyNDExLCJpYXQiOjE3MTQ1NTY2OTJ9.AdHjdw06R4rgVm81B7cQNg58keOWnqnzJbL1vh63hDU';
+
+let storage, getDownloadURL, ref, db;
+
+await import("../firestore.js")
+    .then((module) => {
+        storage = module.storage;
+        getDownloadURL = module.getDownloadURL;
+        ref = module.ref;
+    })
+    .catch((error) => {
+        // console.log(error);
+    });
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Cesium.Viewer('cesiumContainer', {
+    //Terrain is ellipsoid
+    terrain: Cesium.EllipsoidTerrainProvider(),
+    // imageryProvider: false,
+    // baseLayerPicker: false,
+    requestRenderMode: true,
+    geocoder: false,
+    // globe: false,
+    depthTestAgainstTerrain: false,
+    //hide ui
+    animation: false,
+    timeline: false,
+    fullscreenButton: false,
+    vrButton: false,
+    // homeButton: false,
+    sceneModePicker: false,
+    // navigationHelpButton: false,
+    navigationInstructionsInitiallyVisible: false,
+    // selectionIndicator: false,
+    infoBox: false,
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+// const buildingTileset = await Cesium.createOsmBuildingsAsync();
+// viewer.scene.primitives.add(buildingTileset);
+
+// GOOGLE 
+// Enable simultaneous requests.
+// Cesium.RequestScheduler.requestsByServer["tile.googleapis.com:443"] = 18;
+
+
+let CurrentLabel, CurrentPhoto;
+const pinBuilder = new Cesium.PinBuilder();
+const url = Cesium.buildModuleUrl("Assets/Textures/maki/park.png");
+
+// Fly to a location
+const flyTo = (pos) => {
+    // let degrees = Cesium.Cartographic.fromCartesian(pos);
+    // console.log(degrees);
+    const origMagnitude = Cesium.Cartesian3.magnitude(pos);
+    const verticalAmount = 5000000;
+    const newMagnitude = origMagnitude + verticalAmount;
+    const scalar = newMagnitude / origMagnitude;
+
+    const newPosition = new Cesium.Cartesian3();
+    Cesium.Cartesian3.multiplyByScalar(pos, scalar, newPosition);
+
+    viewer.camera.flyTo({
+        destination: newPosition,
+        duration: 1,
+    });
+}
+
+const zoomIn = (pos) => {
+    const origMagnitude = Cesium.Cartesian3.magnitude(pos);
+    const verticalAmount = 5000;
+    const newMagnitude = origMagnitude + verticalAmount;
+    const scalar = newMagnitude / origMagnitude;
+
+    const newPosition = new Cesium.Cartesian3();
+    Cesium.Cartesian3.multiplyByScalar(pos, scalar, newPosition);
+
+    viewer.camera.flyTo({
+        destination: newPosition,
+        duration: 1,
+    });
+
+}
+
+//Add pins
+function addPins(ginkos) {
+    db = ginkos;
+    Object.keys(ginkos).forEach((id) => {
+        // Add pins
+        Promise.resolve(
+            pinBuilder.fromUrl(url, Cesium.Color.GREEN, 48)
+        ).then(function (canvas) {
+
+            let ginko = ginkos[id];
+            console.log(ginko);
+            //PIN
+            return viewer.entities.add({
+                description: ginko.photo,
+                name: id,
+                //place the pin on the ground
+                position: Cesium.Cartesian3.fromDegrees(ginko.coord.longitude, ginko.coord.latitude, 1),
+                billboard: {
+                    image: canvas.toDataURL(),
+                    verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                    show: true
+                    // eyeOffset: new Cesium.Cartesian3(0, 0, -1000000),
+                },
+            });
+        });
+    });
+}
+
+// Draw label
+const DrawLabel = (newPosition, id) => {
+
+    //LABEL
+    CurrentLabel = viewer.entities.add({
+        position: newPosition,
+        label: {
+            text: db[id].first_name + " " + db[id].last_name+".",
+            disableDepthTestDistance: Number.POSITIVE_INFINITY,
+            pixelOffset: new Cesium.Cartesian2(0, -50),
+            showBackground: true,
+            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+        }
+    });
+};
+
+const DrawPhoto = (pos, id) => {
+    if (storage != null && db[id].photo != null) {
+        getDownloadURL(ref(storage, 'images/' + db[id].photo))
+            .then((url) => {
+                CurrentPhoto = viewer.entities.add({
+                    position: pos,
+                    billboard: {
+                        image: url,
+                        scale: 0.05,
+                        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                        pixelOffset: new Cesium.Cartesian2(0, -100),
+                    },
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    //PHOTO
+    // else {
+    //     CurrentPhoto = viewer.entities.add({
+    //         position: pos,
+    //         billboard: {
+    //             image: "../ginkgo.png",
+    //             scale: 1,
+    //             verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+    //             pixelOffset: new Cesium.Cartesian2(0, -50),
+    //             // eyeOffset: new Cesium.Cartesian3(0, 0, -1000000),
+    //         },
+    //     });
+    // }
+};
+
+// Event Handler
+viewer.selectedEntityChanged.addEventListener(function (selectedEntity) {
+    if (Cesium.defined(selectedEntity)) {
+        if (Cesium.defined(selectedEntity.name)) {
+            if (Cesium.defined(CurrentLabel))
+                viewer.entities.remove(CurrentLabel);
+            if (Cesium.defined(CurrentPhoto))
+                viewer.entities.remove(CurrentPhoto);
+            let pos = selectedEntity.position.getValue();
+
+            DrawLabel(pos, selectedEntity.name);
+
+            // console.log('Selected ' + selectedEntity.description);
+            // zoomIn(pos, viewer);
+            zoomIn(pos, viewer);
+
+
+            //Get PHOTO
+            DrawPhoto(pos, selectedEntity.name);
+
+        } else {
+            console.log('Unknown entity selected.');
+        }
+    } else {
+        console.log('Deselected.');
+        if (Cesium.defined(CurrentLabel))
+            viewer.entities.remove(CurrentLabel);
+        if (Cesium.defined(CurrentPhoto))
+            viewer.entities.remove(CurrentPhoto);
+        viewer.selectedEntity = undefined;
+        //remove rotate center point
+    }
+});
+
+export { viewer, flyTo, zoomIn, DrawLabel, addPins };
--- GoogleEarth/index.js
+++ GoogleEarth/index.js
@@ -1,209 +1,62 @@
-//Fire Store
-import { app, analytics, auth, getFirestore, collection, getDocs, signInAnonymously, storage, getDownloadURL, ref } from "../firestore.js";
+//Cesium 
+import { viewer, flyTo, addPins } from "./cesium.js";
 
+//Fire Store 
+let app, analytics, auth, getFirestore, collection, getDocs, signInAnonymously, storage, getDownloadURL, ref;
 
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODE1NTVjOS02MzI1LTQ4NTUtYmJmNS1kODAxNzRmODY2MjkiLCJpZCI6MjEyNDExLCJpYXQiOjE3MTQ1NTY2OTJ9.AdHjdw06R4rgVm81B7cQNg58keOWnqnzJbL1vh63hDU';
-
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Cesium.Viewer('cesiumContainer', {
-    //Terrain is ellipsoid
-    terrain: Cesium.EllipsoidTerrainProvider(),
-    // imageryProvider: false,
-    // baseLayerPicker: false,
-    requestRenderMode: true,
-    geocoder: false,
-    // globe: false,
-    depthTestAgainstTerrain: false,
-    //hide ui
-    animation: false,
-    timeline: false,
-    fullscreenButton: false,
-    vrButton: false,
-    // homeButton: false,
-    sceneModePicker: false,
-    // navigationHelpButton: false,
-    navigationInstructionsInitiallyVisible: false,
-    // selectionIndicator: false,
-    infoBox: false,   
-});    
-
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-// const buildingTileset = await Cesium.createOsmBuildingsAsync();
-// viewer.scene.primitives.add(buildingTileset);
-
-// GOOGLE 
-// Enable simultaneous requests.
-// Cesium.RequestScheduler.requestsByServer["tile.googleapis.com:443"] = 18;
-
+await import("../firestore.js")
+    .then((module) => {
+        app = module.app;
+        analytics = module.analytics;
+        auth = module.auth;
+        getFirestore = module.getFirestore;
+        collection = module.collection;
+        getDocs = module.getDocs;
+        signInAnonymously = module.signInAnonymously;
+        storage = module.storage;
+        getDownloadURL = module.getDownloadURL;
+        ref = module.ref;
+    })
+    .catch((error) => {
+        // console.log(error);
+    });
 
 // Add 3D Tiles tileset.
+// verifie if env.js exists
+let MapKey, ginkos, local;
+
+await import("../env.js")
+    .then((module) => {
+        MapKey = module.MapKey;
+        ginkos = module.ginkos;
+        local = true;
+    })
+    .catch((error) => {
+        MapKey = "AIzaSyAKghInYGx9TlGfhxmxy_VuAG-SfML2N8Q";
+        local = false;
+    });
+
+
 // const tileset = viewer.scene.primitives.add(
 //     new Cesium.Cesium3DTileset({
-//         url: "https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyAKghInYGx9TlGfhxmxy_VuAG-SfML2N8Q",
+//         url: "https://tile.googleapis.com/v1/3dtiles/root.json?key=" + MapKey,
 
 //         // This property is required to display attributions as required.
 //         showCreditsOnScreen: true,
 //     })
 // );
 
-let CurrentLabel;
-let CurrentPhoto;
-const pinBuilder = new Cesium.PinBuilder();
-const url = Cesium.buildModuleUrl("Assets/Textures/maki/park.png");
-
-//Helpers
-//Fly to france while showing the globe
-const flyTo = (pos) => {
-    // let degrees = Cesium.Cartographic.fromCartesian(pos);
-    // console.log(degrees);
-    const origMagnitude = Cesium.Cartesian3.magnitude(pos);
-    const verticalAmount = 5000000;
-    const newMagnitude = origMagnitude + verticalAmount;
-    const scalar = newMagnitude / origMagnitude;
-
-    const newPosition = new Cesium.Cartesian3();
-    Cesium.Cartesian3.multiplyByScalar(pos, scalar, newPosition);
-
-    viewer.camera.flyTo({
-        destination: newPosition,
-        duration: 1,
-    });
-}
-
-const zoomIn = (pos) => {
-    //roate around the center
-    
-}
-
-// Draw label
-const DrawLabel = (newPosition, labelText) => {
-
-    //LABEL
-    CurrentLabel = viewer.entities.add({
-        position: newPosition,
-        label: {
-            text: labelText,
-            disableDepthTestDistance: Number.POSITIVE_INFINITY,
-            pixelOffset: new Cesium.Cartesian2(0, -50),
-            showBackground: true,
-            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-        }
-    });
-};
-
-
-//Selected Event Handler
-viewer.selectedEntityChanged.addEventListener(function (selectedEntity) {
-    if (Cesium.defined(selectedEntity)) {
-        if (Cesium.defined(selectedEntity.name)) {
-            if (Cesium.defined(CurrentLabel))
-                viewer.entities.remove(CurrentLabel);
-            if (Cesium.defined(CurrentPhoto))
-                viewer.entities.remove(CurrentPhoto);
-            let pos = selectedEntity.position.getValue();
-
-            DrawLabel(pos, selectedEntity.name);
-            
-            console.log('Selected ' + selectedEntity.name);
-            // zoomIn(pos, viewer);
-            flyTo(pos, viewer);
-
-
-            //Get PHOTO
-            getDownloadURL(ref(storage,'https://firebasestorage.googleapis.com/v0/b/foret-hubert-reeves-419711.appspot.com/o/images%2FPALOTAI%20Greta%20Montreuil%20%202024%2005%2001.jpeg?alt=media&token=6f6e674d-3342-4aee-ab56-2d8feb154406'))
-                .then((url) => {
-                    CurrentPhoto = viewer.entities.add({
-                        position: pos,
-                        billboard: {
-                            image: url,
-                            scale: 0.05,
-                            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                            pixelOffset: new Cesium.Cartesian2(0, -100),
-                        },
-                    });
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-
-            //PHOTO
-            // CurrentPhoto = viewer.entities.add({
-            //     position: pos,
-            //     billboard: {
-            //         image: "../ginkgo.png",
-            //         scale: 1,
-            //         verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-            //         pixelOffset: new Cesium.Cartesian2(0, -50),
-            //         // eyeOffset: new Cesium.Cartesian3(0, 0, -1000000),
-            //     },
-            // });
-
-        } else {
-            console.log('Unknown entity selected.');
-        }
-    } else {
-        console.log('Deselected.');
-        if (Cesium.defined(CurrentLabel))
-            viewer.entities.remove(CurrentLabel);
-        if (Cesium.defined(CurrentPhoto))
-            viewer.entities.remove(CurrentPhoto);
-        viewer.selectedEntity = undefined;
-        //remove rotate center point
-    }
-});
-
-
-// ginkos.forEach((ginko) => {
-//     // Add pins
-//     Promise.resolve(
-//         pinBuilder.fromUrl(url, Cesium.Color.GREEN, 48)
-//     ).then(function (canvas) {
-
-//         //PIN
-//         return viewer.entities.add({
-//             name: ginko.first_name,
-//             //place the pin on the ground
-//             position: Cesium.Cartesian3.fromDegrees(ginko.coord.longitude, ginko.coord.latitude),
-//             billboard: {
-//                 image: canvas.toDataURL(),
-//                 verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-//                 show: true
-//                 // eyeOffset: new Cesium.Cartesian3(0, 0, -1000000),
-//             },
-//         });
-//     });
-// });
-
-function getGinko() {
-
+function getFirebaseGinko() {
     signInAnonymously(auth)
         .then(async () => {
             const db = getFirestore(app);
             const querySnapshot = await getDocs(collection(db, "ginko"));
             //all to json
-            const ginkos = querySnapshot.docs.map(doc => doc.data());
-
-            ginkos.forEach((ginko) => {
-                // Add pins
-                Promise.resolve(
-                    pinBuilder.fromUrl(url, Cesium.Color.GREEN, 48)
-                ).then(function (canvas) {
-
-                    //PIN
-                    return viewer.entities.add({
-                        name: ginko.first_name,
-                        //place the pin on the ground
-                        position: Cesium.Cartesian3.fromDegrees(ginko.coord.longitude, ginko.coord.latitude, 1),
-                        billboard: {
-                            image: canvas.toDataURL(),
-                            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                            show: true
-                            // eyeOffset: new Cesium.Cartesian3(0, 0, -1000000),
-                        },
-                    });
-                });
+            let ginkos = {};
+            await querySnapshot.docs.forEach((doc) => {
+                ginkos[doc.id] = doc.data();
             });
-
+            addPins(ginkos);
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -212,7 +65,10 @@ function getGinko() {
         });
 }
 
-getGinko();
+console.log("Local", local);
+
+if(local) addPins(ginkos);
+else getFirebaseGinko();
 
 //focus on France
 flyTo(Cesium.Cartesian3.fromDegrees(2.213749, 46.227638, 1), viewer);
